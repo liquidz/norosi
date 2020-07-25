a0249b1f204d0a1fdda6e9a8130ec6e3968e6316
liquidz <liquidz.uo@gmail.com>
2020-07-26 06:24:11 +0900

    Add logo

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,10 @@
 (ns user
   (:require
-   [norosi.core :as core]
-   [com.stuartsierra.component :as component]))
+   [clojure.data.json :as json]
+   [com.stuartsierra.component :as component]
+   [norosi.block.core :as b.core]
+   [norosi.color :as color]
+   [norosi.core :as core]))
 
 (defonce system (atom nil))
 
@@ -27,3 +30,23 @@
   []
   (stop)
   (start))
+
+(def logo
+  (let [_ (assoc b.core/empty-block :color (color/parse-color-code "727171"))
+        a (assoc b.core/fill-block :color (color/parse-color-code "51A8DD"))
+        c (assoc b.core/fill-block :color (color/parse-color-code "D0104C"))
+        e (assoc b.core/fill-block :color (color/parse-color-code "FFB11B"))
+        m (assoc b.core/fill-block :color (color/parse-color-code "7BA23F"))
+        n (assoc b.core/fill-block :color (color/parse-color-code "6A4C9C"))]
+    {:width 26
+     :height 5
+     :blocks [_ a _ _ a _ c c c _ e e e _ c c c _ m m m _ n n n _
+              _ a a _ a _ c _ c _ e _ e _ c _ c _ m _ _ _ _ n _ _
+              _ a a _ a _ c _ c _ e e e _ c _ c _ m m m _ _ n _ _
+              _ a _ a a _ c _ c _ e e _ _ c _ c _ _ _ m _ _ n _ _
+              _ a _ _ a _ c c c _ e _ e _ c c c _ m m m _ n n n _]}))
+
+(comment
+(->> (update logo :blocks (fn [blocks] (map #(update % :content str) blocks)))
+     (json/write-str)
+     (spit "logo.json")))
